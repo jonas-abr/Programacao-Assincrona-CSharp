9d5575d4b919347dfe64b3a3ded0c7fdf222b23c
Jonas Abreu <jonasdesenvolvedorfront@gmail.com>
2021-08-02 17:21:51 -0300

    estudando programação assincrona

--- Assincrono/Assincrono/Program.cs
+++ Assincrono/Assincrono/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace Assincrono
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Tecle algo para iniciar...\n ");
+            Console.ReadKey();
+
+            await Aguardar(5);
+
+            Console.WriteLine("Já passou 5 segundos...\n");
+            Console.WriteLine("Fim");
+            Console.ReadKey();
+
+            static async Task Aguardar(int tempo)
+            {
+                Console.WriteLine("Iniciando espera...");
+                await Task.Delay(TimeSpan.FromSeconds(tempo));
+                Console.WriteLine("Fim da espera...");
+            }
         }
     }
 }
